c7ab4c64a4b417c6f324bb036d499ac60090223a
Kristoffer Stensbo-Smidt <digitaldingo@linux.com>
2015-10-09 23:27:26 +0200

    mergesort.py now fits theoretical complexity

--- week1/mergesort.py
+++ week1/mergesort.py
@@ -1,6 +1,7 @@
 import time
 
 import numpy as np
+from scipy.optimize import curve_fit
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -40,13 +41,16 @@ def mergesort(lst):
 
     return output
 
+def complexity(n, p):
+    return p*n*(np.log2(n) + 1)
+
 
 if __name__ == "__main__":
 
-    timings = 10
+    timings = 20        # repetitions
+    max_npower = 20     # max size of array: 2**max_npower
 
-    #n_list = list(range(100, 2000, 50))
-    n_list = np.logspace(2, 5, 20, dtype=int)
+    n_list = np.logspace(2, max_npower, 20, dtype=int, base=2)
     meantimes = []
     stdtimes = []
 
@@ -66,14 +70,24 @@ if __name__ == "__main__":
 
     print("Looping took {:g} s".format(time.time()-loop_time))
 
+    # Fit theoretical complexity:
+    p = curve_fit(complexity, n_list, meantimes, sigma=stdtimes)[0][0]
+    x = np.linspace(0, 2**max_npower, 100)
+    y = complexity(x, p)
+
     fig, ax = plt.subplots()
 
+    # Plot measurements:
     ax.errorbar(n_list, meantimes, stdtimes, fmt='none', ecolor=seapal[0])
     ax.plot(n_list, meantimes, ls='', marker="o", ms=6)
 
-    ax.set_title("Merge sort running time ({} repititions)".format(timings))
+    # Plot complexity:
+    ax.plot(x, y, alpha=.7)
+
+    ax.set_title("Merge sort running time ({} repetitions)".format(timings))
     ax.set_xlabel("Array size")
     ax.set_ylabel("Running time")
+    ax.set_xscale('log', basex=2)
 
     plt.tight_layout()
     plt.show(block=False)
